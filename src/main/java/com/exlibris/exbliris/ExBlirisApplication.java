a4ba0821d885a7430718c924bfcb238325a0b337
Nights-Wolf <dawinavi@gmail.com>
2023-01-15 16:03:51 +0100

    Init

---
+++ src/main/java/com/exlibris/exbliris/ExBlirisApplication.java
@@ -0,0 +1,13 @@
+package com.exlibris.exbliris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExBlirisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExBlirisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exlibris/exbliris/ExBlirisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exlibris.exbliris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExBlirisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
