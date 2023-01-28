a7f42ab8a51c1b6f1feb2787b3e8e70afcbd64f5
Nights-Wolf <dawinavi@gmail.com>
2023-01-28 02:02:18 +0100

    Library rest controller added. Getting library by user created in service, repository and controller. Try/catch block added in user rest controller (delete user).

--- src/main/java/com/exlibris/exbliris/database/LibraryRepository.java
+++ src/main/java/com/exlibris/exbliris/database/LibraryRepository.java
@@ -1,9 +1,14 @@
 package com.exlibris.exbliris.database;
 
 import com.exlibris.exbliris.models.Library;
+import com.exlibris.exbliris.models.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LibraryRepository extends JpaRepository<Library, Long> {
+
+    List<Library> getByUserId(User user);
 }
