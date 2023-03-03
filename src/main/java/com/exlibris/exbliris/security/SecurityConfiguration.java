ea00a1ce6c93fa770009c30d6f19ebeb2a9278c6
Nights-Wolf <dawinavi@gmail.com>
2023-03-03 18:36:26 +0100

    Spring security added to project.

---
+++ src/main/java/com/exlibris/exbliris/security/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.exlibris.exbliris.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+}
